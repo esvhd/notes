474fd454efbd488408852375c83f23921b85d542
esvhd <7330385+esvhd@users.noreply.github.com>
2019-02-23 22:51:08 +0100

    pip changes

--- ubuntu/scripts/conda.sh
+++ ubuntu/scripts/conda.sh
@@ -6,3 +6,8 @@ rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 
 # enable extenstions
 jupyter nbextensions_configurator enable --user
+
+# a few packages can be installed from github source in editable mode
+# filterpy
+# bastage/arch
+# fastai
